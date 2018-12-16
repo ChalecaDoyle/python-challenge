b7ea00d119c96efa0c13281e8e63181e8408bd9e
ChalecaDoyle <40638401+ChalecaDoyle@users.noreply.github.com>
2018-12-15 20:44:35 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,25 +1,43 @@
- import os
- import csv
- 
- # open the file using "write" mode. Specify the variables to hold the contents
-with open(csvpath) as csvfile:
-    
-    total_votes = 0
-    +vote_count =[]
-    +1=[]
-    
-    #Get all input list of csv files in C:\Users\doyle\Desktop\python-challenge\PyPoll\Resources
+ # create a dataset 
+import os
+import csv
 
-    #Initialize csv.reader
+#csvpath = os.path.join()
+#candidates = []
+#andidate_votes = []
+
+
+voterid = os.path.join("Resources", "election_data.csv")
+
+
+# open the file using "write" mode. Specify the variables to hold the contents
+with open(voterid) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
-    next(csvreader) 
+    # do not include headers
+    csv_header = next(csvfile)
     
-    for rows in csvreader:
-        
-        profits.append(int(rows[1]))
-        months.append(rows[0])
-        sum_profits = sum(profits)
-        row_count =len(months)
-        
-    print(str(sum_profits))
+    #lists  and variables
+    candidate =[]
+    candidate_votes ={}
+    total_votes = 0
+    Percentage = 0
+    winners = []
+    
+    
+    
+
+    #Initialize csv.reader
+    #reader = csv.reader(election_data)
+    #skip header
+    #next(csvreader)
+    
+    
+    for r in csvreader:
+        total_votes = total_votes + 1
+        candidate_name=r[2]
+        if(candidate_name not in candidate):
+            candidate.append(candidate_name)
+            candidate_votes[candidate_name] = 0        
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+       
